4af871bd77ed007807a40bec429fab3e46b04146
EliMine6915 <elias.mueller10@outlook.de>
2025-12-28 20:01:07 +0100

    Removed a testing URI

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const PORT = process.env.PORT || 3001;
 // MIDDLEWARE
 // ============================================
 app.use(cors({
-  origin: ['https://trackify-kayh.onrender.com', 'http://127.0.0.1:3001'],
+  origin: ['https://trackify-kayh.onrender.com'],
   credentials: true
 }));
 app.use(express.json());
